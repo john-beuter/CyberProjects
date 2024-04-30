3b420f895dc7c6044f270c90c0f25346165c9bd5
Johnathon Beuter <johnathonbeuter@gmail.com>
2024-04-30 17:57:20 -0500

    Starting a port scanner in GO

---
+++ Port Scanner/portScanner.go
@@ -0,0 +1,25 @@
+package main
+
+import(
+	"net"
+	"fmt"
+	//"strings"
+)
+
+
+
+//Function should take an IP address as a parameter and enumerate that IP
+//Blast it with packets and listen for the connection?
+func scanIP(address int){
+	//Only listen back on a select IP
+	
+
+}
+
+
+
+
+
+func main(){
+	println("test")
+}
