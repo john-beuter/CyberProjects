84e481ec19760c89978ea65cbfab4be90a72fa4f
Johnathon Beuter <johnathonbeuter@gmail.com>
2024-04-30 20:26:21 -0500

    Prototyping and experimenting

--- Port Scanner/portScanner.go
+++ Port Scanner/portScanner.go
@@ -1,25 +1,24 @@
 package main
 
-import(
+import (
 	"net"
-	"fmt"
-	//"strings"
+	//"fmt"
+	"strconv"
 )
 
+// Function should take an IP address as a parameter and enumerate that IP
+// Blast it with packets and listen for the connection?
+func scanIP(address string) {
 
+	for i := 0; i < 1000; i++ {
+		s := net.JoinHostPort(address, strconv.Itoa(i))
+		net.Dial("tcp", s) //Trying to make connection
+	}
 
-//Function should take an IP address as a parameter and enumerate that IP
-//Blast it with packets and listen for the connection?
-func scanIP(address int){
 	//Only listen back on a select IP
-	
 
 }
 
-
-
-
-
-func main(){
+func main() {
 	println("test")
 }
