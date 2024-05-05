0116374c8b3a84e9167646714e85f8c61c0b63b0
Johnathon Beuter <johnathonbeuter@gmail.com>
2024-05-05 16:18:36 -0500

    Making changes to use localhost for testing

--- Port Scanner/Server/server.go
+++ Port Scanner/Server/server.go
@@ -1,37 +1,34 @@
 package main
 
-import(
+import (
 	"fmt"
 	"net"
 )
 
-func main(){
+func main() {
 
-	listener, err := net.Listen("tcp", ":8888")
-	if err != nil{
+	listener, err := net.Listen("tcp", ":222")
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	defer listener.Close()
 
-
 	connection, err := listener.Accept()
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-
 	response := []byte("Hello from the server")
 	_, err = connection.Write(response)
 
-	if err != nil{
+	if err != nil {
 		fmt.Println("write error", err) // this is how we can print the var
 		return
 	}
 
 	connection.Close()
-
 
 }
--- Port Scanner/portScanner.go
+++ Port Scanner/portScanner.go
@@ -7,36 +7,34 @@ import (
 	"sync"
 )
 
-
 var wait sync.WaitGroup
 
-func scanPort(host string){
+func scanPort(host string) {
 	connect, err := net.Dial("tcp", host) //Trying to make connection
-	port := host[len(host)-2:]
+	port := host[len(host)-3:]
 	if err != nil {
-			//This would print each port that we failed to connect to 
+		//This would print each port that we failed to connect to
+		fmt.Println("Port " + port + " is closed")
 	}
-		
-	if connect != nil{ // IF we have a connection, then record that
+
+	if connect != nil { // IF we have a connection, then record that
 		fmt.Println("Port " + port + " is open")
 	}
 
-	defer wait.Done()
-} 
-
-
+	defer wait.Done() //When the function is done then decrement 
+}
 
 // Function should take an IP address as a parameter and enumerate that IP
 func scanIP(address string) {
-	for i := 0; i < 24; i++ {
+	for i := 200; i < 224; i++ {
 		wait.Add(1)
 		port := strconv.Itoa(i)
 		s := net.JoinHostPort(address, port)
 		go scanPort(s)
-	}	
-	wait.Wait() //Got it to somewhat work with goroutines but having deadlock!
+	}
+	wait.Wait() 
 }
 
 func main() {
-	scanIP("192.168.4.76")
+	scanIP("localhost")
 }
