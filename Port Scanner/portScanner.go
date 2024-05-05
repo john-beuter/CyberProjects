dcfa56e2ce5de7b10b05c3c39f93210338e585a4
Johnathon Beuter <johnathonbeuter@gmail.com>
2024-05-04 20:20:29 -0500

    Broke it again

--- Port Scanner/portScanner.go
+++ Port Scanner/portScanner.go
@@ -8,24 +8,26 @@ import (
 )
 
 
+var wait sync.WaitGroup
+
 func scanPort(host string){
-		connect, err := net.Dial("tcp", host) //Trying to make connection
-		port := host[len(host)-2:]
-	
-		if err != nil {
+	connect, err := net.Dial("tcp", host) //Trying to make connection
+	port := host[len(host)-2:]
+	if err != nil {
 			//This would print each port that we failed to connect to 
-		}
+	}
 		
-		if connect != nil{ // IF we have a connection, then record that
-			fmt.Println("Port " + port + " is open")
-		}
+	if connect != nil{ // IF we have a connection, then record that
+		fmt.Println("Port " + port + " is open")
+	}
+
+//	defer wait.Done()
 } 
 
 
 
 // Function should take an IP address as a parameter and enumerate that IP
 func scanIP(address string) {
-	var wait sync.WaitGroup
 	for i := 0; i < 24; i++ {
 		wait.Add(1)
 		port := strconv.Itoa(i)
