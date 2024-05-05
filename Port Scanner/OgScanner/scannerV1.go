13ef56ad3b38c0ee022cf4a352370bc919ba0364
Johnathon Beuter <johnathonbeuter@gmail.com>
2024-05-05 17:19:41 -0500

    Splitting ideas

---
+++ Port Scanner/OgScanner/scannerV1.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"sync"
+)
+
+var wait sync.WaitGroup
+
+func scanPort(host string, c chan string) {
+	connect, err := net.Dial("tcp", host) //Trying to make connection
+	port := host[len(host)-3:]
+	var output string
+	if err != nil {
+		//This would print each port that we failed to connect to
+		//fmt.Println("Port " + port + " is closed")
+		output = port + " is closed"
+	}
+
+	if connect != nil { // IF we have a connection, then record that
+		//fmt.Println("Port " + port + " is open")
+		output = port + " is open"
+	}
+	
+	c <- output
+	//close(c)
+//	defer wait.Done() //When the function is done then decrement 
+}
+
+// Function should take an IP address as a parameter and enumerate that IP
+func scanIP(address string) {
+	// for i := 200; i < 224; i++ {
+	// 	wait.Add(1)
+	// 	port := strconv.Itoa(i)
+	// 	s := net.JoinHostPort(address, port)
+	// 	go scanPort(s)
+	// }
+	// wait.Wait()
+
+	c := make(chan string, 22)
+	for i := 200; i < 224; i++{
+		port := strconv.Itoa(i)		
+	 	s := net.JoinHostPort(address, port)
+		go scanPort(s, c)
+	}
+	for i := range c{
+		fmt.Println(i)
+	}
+}
+
+func main() {
+	scanIP("localhost")
+}
--- Port Scanner/portScanner.go
+++ Port Scanner/portScanner.go
@@ -9,24 +9,29 @@ import (
 
 var wait sync.WaitGroup
 
-func scanPort(host string, c chan string) {
-	connect, err := net.Dial("tcp", host) //Trying to make connection
-	port := host[len(host)-3:]
-	var output string
-	if err != nil {
-		//This would print each port that we failed to connect to
-		//fmt.Println("Port " + port + " is closed")
-		output = port + " is closed"
-	}
+func scanPort(port int, c chan string, address string) {
 
-	if connect != nil { // IF we have a connection, then record that
-		//fmt.Println("Port " + port + " is open")
-		output = port + " is open"
+	for i := range port {
+		portValue := strconv.Itoa(i)
+		host := net.JoinHostPort(address, portValue)
+		connect, err := net.Dial("tcp", host) //Trying to make connection
+		portValue = host[len(host)-2:]
+		var output string
+		if err != nil {
+			//This would print each port that we failed to connect to
+			//fmt.Println("Port " + port + " is closed")
+			output = portValue + " is closed"
+		}
+
+		if connect != nil { // IF we have a connection, then record that
+			//fmt.Println("Port " + port + " is open")
+			output = portValue + " is open"
+		}
+
+		c <- output
 	}
-	
-	c <- output
 	//close(c)
-//	defer wait.Done() //When the function is done then decrement 
+	//	defer wait.Done() //When the function is done then decrement
 }
 
 // Function should take an IP address as a parameter and enumerate that IP
@@ -39,13 +44,19 @@ func scanIP(address string) {
 	// }
 	// wait.Wait()
 
-	c := make(chan string, 22)
-	for i := 200; i < 224; i++{
-		port := strconv.Itoa(i)		
-	 	s := net.JoinHostPort(address, port)
-		go scanPort(s, c)
-	}
-	for i := range c{
+	//Make the second parameter of make be the ports
+	//Could use _ ?
+
+	c := make(chan string, 24)
+	go scanPort(cap(c), c, address) //use the first in as a parameter for the loop.
+
+	// for i := 200; i < 224; i++ {
+	// 	port := strconv.Itoa(i)
+	// 	s := net.JoinHostPort(address, port)
+	// 	fmt.Println(i - 200)
+	// 	go scanPort(s, c)
+	// }
+	for i := range c {
 		fmt.Println(i)
 	}
 }
