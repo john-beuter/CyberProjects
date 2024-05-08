39e223ae7894be7911118203ecc8e87b40ef6528
Johnathon Beuter <johnathonbeuter@gmail.com>
2024-05-08 18:11:04 -0500

    Making small formatting improvements

--- Port Scanner/OgScanner/scannerV1.go
+++ Port Scanner/OgScanner/scannerV1.go
@@ -11,17 +11,15 @@ var wait sync.WaitGroup
 
 func scanPort(host string) {
 	connect, err := net.Dial("tcp", host) //Trying to make connection
-	port := host[len(host)-3:]
+	port := host[len(host)-3:] //I could string strip the : 
 	var output string
 	if err != nil {
-		//This would print each port that we failed to connect to
-		//fmt.Println("Port " + port + " is closed")
 		output = port + " is closed"
-	}
-
-	if connect != nil { // IF we have a connection, then record that
+	}else{ // IF we have a connection, then record that
 		//fmt.Println("Port " + port + " is open")
 		output = port + " is open"
+		connect.Close()
+		//fmt.Sprintf("%s is open", port)
 	}
 
 	fmt.Println(output)
