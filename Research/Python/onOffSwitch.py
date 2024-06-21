8798d2f2e657f44072e356ed97f77fd9d4aaa57c
Johnathon Beuter <johnathonbeuter@gmail.com>
2024-06-21 13:31:54 -0400

    Modifying GO code

--- Research/Python/onOffSwitch.py
+++ Research/Python/onOffSwitch.py
@@ -4,4 +4,4 @@ from pyModbusTCP.client import ModbusClient
 # init
 c = ModbusClient(host='192.168.13.86', port=502, auto_open=True, debug=False)
 
-c.write_single_coil(0, 0)
+c.write_single_coil(0, 1)
